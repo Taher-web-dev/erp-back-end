d56fe84cccc4e6f812ef6dd02892fe860a9fe0d4
Taher <ttaherhagui@gmail.com>
2022-07-10 16:53:21 +0100

    add constant to session controller.

--- app/controllers/v1/session_controller.rb
+++ app/controllers/v1/session_controller.rb
@@ -1,14 +1,16 @@
-require_relative('../helpers/responses_helper')
-require_relative('../helpers/errors')
+#require_relative('../helpers/responses_helper')
+#require_relative('../helpers/errors')
 
 class V1::SessionController < ApplicationController
+  Status = Helpers::ResponsesHelper::Status
+  Errors = Helpers::Errors
   def create
     user = User.find_by(username: login_params['username'])
 
     if user&.authenticate(login_params['password'])
       render json: { status: Status.success, data: user }, status: :ok
     else
-      render json: { status: Status.failed, error: WRONG_CREDENTIALS }, status: :unauthorized
+      render json: { status: Status.failed, error: Errors::WRONG_CREDENTIALS }, status: :unauthorized
     end
   end
 
